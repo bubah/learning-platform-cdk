2975dd877cc8f281b08394e496b80715dfec1457
Bubah Conteh <contehbub@gmail.com>
2025-04-09 16:44:06 -0400

    Initial commit

---
+++ test/learning-platform-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LearningPlatformCdk from '../lib/learning-platform-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/learning-platform-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LearningPlatformCdk.LearningPlatformCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
