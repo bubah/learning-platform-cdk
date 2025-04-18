4b6429e31e3b2ae7715a612a1ceb3211d0adb898
Bubah Conteh <contehbub@gmail.com>
2025-04-18 14:28:16 -0400

    Adds linting to project

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import js from "@eslint/js";
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+    plugins: { js },
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+    },
+    extends: ["js/recommended"],
+    rules: {
+      "no-console": "warn",
+      "no-unused-vars": "warn",
+    },
+  },
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.json"],
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/explicit-function-return-type": "off",
+    },
+  },
+]);
